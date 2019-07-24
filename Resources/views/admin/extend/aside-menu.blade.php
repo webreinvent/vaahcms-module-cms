ee96cb91485632a77a7d94316fcb913af97600f8
Pradeep <webreinvent@gmail.com>
2019-07-24 13:56:10 +0530

    aside menu restructured

--- Resources/views/admin/extend/aside-menu.blade.php
+++ Resources/views/admin/extend/aside-menu.blade.php
@@ -1,6 +1,13 @@
 <li class="nav-label  mg-t-25">Content</li>
 
-<li class="nav-item"><a href="{{route("vh.cms.pages")}}" class="nav-link">
-        <i data-feather="file-text"></i> <span>CMS</span>
+
+<li class="nav-item"><a href="{{route("vh.cms")}}#/pages" class="nav-link">
+        <i data-feather="file-text"></i> <span>Pages</span>
+    </a>
+</li>
+
+
+<li class="nav-item"><a href="{{route("vh.cms")}}#/menus" class="nav-link">
+        <i data-feather="link-2"></i> <span>Menu</span>
     </a>
 </li>
