9d6b4a1c82810b6093fe620177838dccc42d1577
vikram chand <vikram.wri231@webreinvent.com>
2020-10-27 16:25:54 +0530

    fix: bulk action

--- Vue/pages/content-types/partials/ListLargeView.vue
+++ Vue/pages/content-types/partials/ListLargeView.vue
@@ -2,6 +2,7 @@
 <template>
     <div>
         <b-table :data="page.list_is_empty ? [] : page.list.data"
+                 :checkable="true"
                  :checked-rows.sync="page.bulk_action.selected_items"
                  checkbox-position="left"
                  :hoverable="true"
