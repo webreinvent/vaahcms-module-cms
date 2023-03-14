b32bc48b0f3461ca8e7eee213f542e1852b8022e
Vikram <vikram.wri231@webreinvent.com>
2023-03-14 19:31:47 +0530

    Fixed: relation content issue

--- Entities/Content.php
+++ Entities/Content.php
@@ -281,7 +281,10 @@ class Content extends Model {
 
                             $relation =  vh_content_relations_by_name($field['meta']['type']);
 
-                            if($relation && isset($relation['namespace']) && $relation['namespace']){
+                            if($relation && isset($relation['namespace'])
+                                && $relation['namespace']
+                                && $field && isset($field['content'])
+                                && is_array($field['content'])){
                                 foreach ($field['content'] as $id){
                                     $data = [
                                         'relatable_id' => $id,
