7b3cd4c7bee1965e464b62bb15263e121c081d3a
we-abinash-s001 <abinash.s001@webreinvent.com>
2023-03-20 12:37:34 +0530

    Updated: research on contents section

--- Vue/routes/routes.js
+++ Vue/routes/routes.js
@@ -4,10 +4,12 @@ import dashboard from "./vue-routes-dashboard";
 import blocks from "./vue-routes-blocks";
 import menus from "./vue-routes-menus";
 import contenttypes from "./vue-routes-contenttypes";
+import contents from "./vue-routes-contents";
 
 routes = routes.concat(dashboard);
 routes = routes.concat(blocks);
 routes = routes.concat(menus);
 routes = routes.concat(contenttypes);
+routes = routes.concat(contents);
 
 export default routes;
