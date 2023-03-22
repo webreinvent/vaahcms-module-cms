f48a3951fb793dc3837dccba102ee4d81ff22616
we-abinash-s001 <abinash.s001@webreinvent.com>
2023-03-22 19:01:58 +0530

    Updated: topnav added in cms module

---
+++ Vue/components/Topnav.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {reactive, ref} from 'vue';
+import { useRootStore } from '../stores/root'
+
+import Menubar from 'primevue/menubar';
+const root = useRootStore();
+const inputs = {
+}
+const data = reactive(inputs);
+const height = ref(window.innerHeight)
+
+const menu = ref();
+
+
+
+</script>
+<template>
+
+    <div v-if="height && root.assets">
+        <Menubar :model="root.assets.aside_menu" />
+    </div>
+
+</template>
+
+
