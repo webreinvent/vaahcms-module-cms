916515b67cc5969b4c50ff101d3f0e05bb603dbf
Vikram <vikram.wri231@webreinvent.com>
2023-11-04 12:34:52 +0530

    Fixed: issue rise with frontend

---
+++ Entities/Content.php
@@ -0,0 +1,404 @@
+<?php namespace VaahCms\Modules\Cms\Models;
+
+use Carbon\Carbon;
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use WebReinvent\VaahCms\Traits\CrudWithUuidObservantTrait;
+use WebReinvent\VaahCms\Models\User;
+
+class Content extends ContentBase
+{
+
+
+
+    //-------------------------------------------------
+    //-------------------------------------------------
+    public function scopeGetSorted($query, $filter)
+    {
+
+        if(!isset($filter['sort']))
+        {
+            return $query->orderBy('id', 'desc');
+        }
+
+        $sort = $filter['sort'];
+
+
+        $direction = Str::contains($sort, ':');
+
+        if(!$direction)
+        {
+            return $query->orderBy($sort, 'asc');
+        }
+
+        $sort = explode(':', $sort);
+
+        return $query->orderBy($sort[0], $sort[1]);
+    }
+    //-------------------------------------------------
+    public function scopeIsActiveFilter($query, $filter)
+    {
+
+        if(!isset($filter['status'])
+            || is_null($filter['status'])
+            || $filter['status'] === 'null'
+        )
+        {
+            return $query;
+        }
+        $status = $filter['status'];
+
+        if($status === 'true' || $status === true)
+        {
+            return $query->whereNotNull('status');
+        } else{
+            return $query->whereNull('status');
+        }
+
+    }
+    //-------------------------------------------------
+    public function scopeTrashedFilter($query, $filter)
+    {
+
+        if(!isset($filter['trashed']))
+        {
+            return $query;
+        }
+        $trashed = $filter['trashed'];
+
+        if($trashed === 'include')
+        {
+            return $query->withTrashed();
+        } else if($trashed === 'only'){
+            return $query->onlyTrashed();
+        }
+
+    }
+    //-------------------------------------------------
+    public function scopeSearchFilter($query, $filter)
+    {
+
+        if(!isset($filter['q']))
+        {
+            return $query;
+        }
+        $search = $filter['q'];
+        $query->where(function ($q) use ($search) {
+            $q->where('name', 'LIKE', '%' . $search . '%')
+                ->orWhere('slug', 'LIKE', '%' . $search . '%');
+        });
+
+    }
+    //-------------------------------------------------
+    public static function getList($request)
+    {
+        $list = self::getSorted($request->filter);
+        $list->where('vh_cms_content_type_id', $request->content_type->id);
+        $list->isActiveFilter($request->filter);
+        $list->trashedFilter($request->filter);
+        $list->searchFilter($request->filter);
+
+        $rows = config('vaahcms.per_page');
+
+        if($request->has('rows'))
+        {
+            $rows = $request->rows;
+        }
+
+        $list = $list->paginate($rows);
+
+        $response['success'] = true;
+        $response['data'] = $list;
+
+        return $response;
+
+
+    }
+
+    //-------------------------------------------------
+    public static function updateList($request)
+    {
+
+        $inputs = $request->all();
+
+        $rules = array(
+            'type' => 'required',
+        );
+
+        $messages = array(
+            'type.required' => 'Action type is required',
+        );
+
+
+        $validator = \Validator::make($inputs, $rules, $messages);
+        if ($validator->fails()) {
+
+            $errors = errorsToArray($validator->errors());
+            $response['success'] = false;
+            $response['errors'] = $errors;
+            return $response;
+        }
+
+        if(isset($inputs['items']))
+        {
+            $items_id = collect($inputs['items'])
+                ->pluck('id')
+                ->toArray();
+        }
+
+
+        $items = self::whereIn('id', $items_id)
+            ->withTrashed();
+
+        switch ($inputs['type']) {
+            case 'deactivate':
+                $items->update(['status' => null]);
+                break;
+            case 'activate':
+                $items->update(['status' => 1]);
+                break;
+            case 'trash':
+                self::whereIn('id', $items_id)->delete();
+                break;
+            case 'restore':
+                self::whereIn('id', $items_id)->restore();
+                break;
+        }
+
+        $response['success'] = true;
+        $response['data'] = true;
+        $response['messages'][] = 'Action was successful.';
+
+        return $response;
+    }
+
+    //-------------------------------------------------
+    public static function deleteList($request): array
+    {
+        $inputs = $request->all();
+
+        $rules = array(
+            'type' => 'required',
+            'items' => 'required',
+        );
+
+        $messages = array(
+            'type.required' => 'Action type is required',
+            'items.required' => 'Select items',
+        );
+
+        $validator = \Validator::make($inputs, $rules, $messages);
+        if ($validator->fails()) {
+
+            $errors = errorsToArray($validator->errors());
+            $response['failed'] = true;
+            $response['messages'] = $errors;
+            return $response;
+        }
+
+        $items_id = collect($inputs['items'])->pluck('id')->toArray();
+        self::whereIn('id', $items_id)->forceDelete();
+
+        $response['success'] = true;
+        $response['data'] = true;
+        $response['messages'][] = 'Action was successful.';
+
+        return $response;
+    }
+    //-------------------------------------------------
+    public static function listAction($request, $type): array
+    {
+        $inputs = $request->all();
+
+        if(isset($inputs['items']))
+        {
+            $items_id = collect($inputs['items'])
+                ->pluck('id')
+                ->toArray();
+
+            $items = self::whereIn('id', $items_id)
+                ->withTrashed();
+        }
+
+
+        switch ($type) {
+            case 'deactivate':
+                if($items->count() > 0) {
+                    $items->update(['status' => null]);
+                }
+                break;
+            case 'activate':
+                if($items->count() > 0) {
+                    $items->update(['status' => 1]);
+                }
+                break;
+            case 'trash':
+                if(isset($items_id) && count($items_id) > 0) {
+                    self::whereIn('id', $items_id)->delete();
+                }
+                break;
+            case 'restore':
+                if(isset($items_id) && count($items_id) > 0) {
+                    self::whereIn('id', $items_id)->restore();
+                }
+                break;
+            case 'delete':
+                if(isset($items_id) && count($items_id) > 0) {
+                    self::whereIn('id', $items_id)->forceDelete();
+                }
+                break;
+            case 'activate-all':
+                self::query()->update(['status' => 1]);
+                break;
+            case 'deactivate-all':
+                self::query()->update(['status' => null]);
+                break;
+            case 'trash-all':
+                self::query()->delete();
+                break;
+            case 'restore-all':
+                self::withTrashed()->restore();
+                break;
+            case 'delete-all':
+                self::withTrashed()->forceDelete();
+                break;
+        }
+
+        $response['success'] = true;
+        $response['data'] = true;
+        $response['messages'][] = 'Action was successful.';
+
+        return $response;
+    }
+    //-------------------------------------------------
+    public static function getItem($id)
+    {
+
+        $item = Content::where('id', $id)
+            ->with([
+                'contentType', 'theme', 'template',
+                'authorUser', 'createdByUser', 'updatedByUser',
+                'deletedByUser',
+                'fields' => function($f){
+                    $f->with(['group', 'field']);
+                }
+            ])
+            ->withTrashed()
+            ->first();
+
+
+        $content_form_groups = static::getFormGroups($item, 'content');
+
+        $template_form_groups = static::getFormGroups($item, 'template');
+
+        $response['success'] = true;
+
+        $item->content_form_groups = $content_form_groups;
+        $item->template_form_groups = $template_form_groups;
+
+        $response['data'] = $item;
+
+        return $response;
+
+    }
+    //-------------------------------------------------
+    public static function updateItem($request, $id)
+    {
+        $inputs = $request->all();
+
+        $validation = self::validation($request);
+        if (!$validation['success']) {
+            return $validation;
+        }
+
+        // check if name exist
+        $item = self::where('id', '!=', $inputs['id'])
+            ->where('vh_cms_content_type_id',$inputs['content_type']['id'])
+            ->withTrashed()
+            ->where('name', $inputs['name'])->first();
+
+        if($item){
+            $response['success'] = false;
+            $response['messages'][] = "This name is already exist.";
+            return $response;
+        }
+
+        $item = self::where('id', $id)->withTrashed()->first();
+        $item->fill($inputs);
+        $item->slug = Str::slug($inputs['slug']);
+        $item->save();
+
+
+
+
+        static::storeFormGroups($item, $inputs['content_form_groups']);
+
+        static::storeFormGroups($item, $inputs['template_form_groups']);
+
+        $response = self::getItem($item->id);
+        $response['messages'][] = 'Saved successfully.';
+        return $response;
+
+    }
+    //-------------------------------------------------
+    public static function deleteItem($request, $id): array
+    {
+        $item = self::where('id', $id)->withTrashed()->first();
+        if (!$item) {
+            $response['success'] = false;
+            $response['messages'][] = 'Record does not exist.';
+            return $response;
+        }
+        $item->forceDelete();
+
+        $response['success'] = true;
+        $response['data'] = [];
+        $response['messages'][] = 'Record has been deleted';
+
+        return $response;
+    }
+    //-------------------------------------------------
+    public static function itemAction($request, $id, $type): array
+    {
+        switch($type)
+        {
+            case 'activate':
+                self::where('id', $id)
+                    ->withTrashed()
+                    ->update(['status' => 1]);
+                break;
+            case 'deactivate':
+                self::where('id', $id)
+                    ->withTrashed()
+                    ->update(['status' => null]);
+                break;
+            case 'trash':
+                self::find($id)->delete();
+                break;
+            case 'restore':
+                self::where('id', $id)
+                    ->withTrashed()
+                    ->restore();
+                break;
+        }
+
+        return self::getItem($id);
+    }
+    //-------------------------------------------------
+
+    //-------------------------------------------------
+    public static function getActiveItems()
+    {
+        $item = self::where('status', 1)
+            ->first();
+        return $item;
+    }
+
+    //-------------------------------------------------
+    //-------------------------------------------------
+    //-------------------------------------------------
+
+
+}
---
+++ Entities/ContentFormField.php
@@ -0,0 +1,193 @@
+<?php namespace VaahCms\Modules\Cms\Models;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use WebReinvent\VaahCms\Models\Taxonomy;
+use WebReinvent\VaahCms\Models\User;
+use WebReinvent\VaahCms\Traits\CrudWithUuidObservantTrait;
+
+class ContentFormField extends Model {
+
+    use SoftDeletes;
+    use CrudWithUuidObservantTrait;
+
+    //-------------------------------------------------
+    protected $table = 'vh_cms_content_form_fields';
+    //-------------------------------------------------
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+    //-------------------------------------------------
+    protected $dateFormat = 'Y-m-d H:i:s';
+    //-------------------------------------------------
+    protected $fillable = [
+        'uuid',
+        'vh_cms_content_id',
+        'vh_cms_form_group_id',
+        'vh_cms_form_field_id',
+        'vh_cms_form_group_index',
+        'content',
+        'meta',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
+
+    //-------------------------------------------------
+
+
+
+    //-------------------------------------------------
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        $date_time_format = config('settings.global.datetime_format');
+
+        return $date->format($date_time_format);
+
+    }
+
+    //-------------------------------------------------
+    public function setContentAttribute($value)
+    {
+        if(is_array($value) || is_object($value))
+        {
+            $this->attributes['content'] = json_encode($value);
+        } else{
+            $this->attributes['content'] = $value;
+        }
+    }
+    //-------------------------------------------------
+    public function getContentAttribute($value)
+    {
+
+        if(!$value)
+        {
+            return null;
+        }
+
+        if($value)
+        {
+
+            if(vh_is_json($value))
+            {
+                return json_decode($value);
+            }
+
+            return $value;
+        }
+
+
+        return null;
+    }
+    //-------------------------------------------------
+    public function setMetaAttribute($value)
+    {
+        if($value)
+        {
+            $this->attributes['meta'] = json_encode($value);
+        }
+        $this->attributes['meta'] = null;
+    }
+    //-------------------------------------------------
+    public function getMetaAttribute($value)
+    {
+        if($value)
+        {
+            return json_decode($value);
+        }
+        return null;
+    }
+    //-------------------------------------------------
+
+    //-------------------------------------------------
+    //-------------------------------------------------
+    public function getTableColumns() {
+        return $this->getConnection()->getSchemaBuilder()
+            ->getColumnListing($this->getTable());
+    }
+    //-------------------------------------------------
+    public function scopeExclude($query, $columns)
+    {
+        return $query->select( array_diff( $this->getTableColumns(),$columns) );
+    }
+    //-------------------------------------------------
+    public function scopeIsPublished($query)
+    {
+        return $query->where( 'is_published', 1 );
+    }
+    //-------------------------------------------------
+    public function createdByUser()
+    {
+        return $this->belongsTo(User::class,
+            'created_by', 'id'
+        )->select('id', 'uuid', 'first_name', 'last_name', 'email');
+    }
+
+    //-------------------------------------------------
+    public function updatedByUser()
+    {
+        return $this->belongsTo(User::class,
+            'updated_by', 'id'
+        )->select('id', 'uuid', 'first_name', 'last_name', 'email');
+    }
+    //-------------------------------------------------
+    public function deletedByUser()
+    {
+        return $this->belongsTo(User::class,
+            'deleted_by', 'id'
+        )->select('id', 'uuid', 'first_name', 'last_name', 'email');
+    }
+    //-------------------------------------------------
+    public function group()
+    {
+        return $this->belongsTo(FormGroup::class,
+            'vh_cms_form_group_id', 'id'
+        );
+    }
+    //-------------------------------------------------
+    public function field()
+    {
+        return $this->belongsTo(FormField::class,
+            'vh_cms_form_field_id', 'id'
+        );
+    }
+    //-------------------------------------------------
+    public function content()
+    {
+        return $this->belongsTo(Content::class,
+            'vh_cms_content_id', 'id'
+        );
+    }
+    //-------------------------------------------------
+    public static function getContentAsset($content,$type)
+    {
+        $value = $content;
+
+        if($content && $type
+            && ($type == 'image' || $type == 'media')
+            && is_string($content))
+        {
+            $value = asset($content);
+        }
+
+        return $value;
+    }
+    //-------------------------------------------------
+    public function taxonomies()
+    {
+        return $this->morphedByMany(Taxonomy::class,
+            'relatable',
+            'vh_cms_content_form_relations',
+            'vh_cms_content_form_field_id');
+    }
+    //-------------------------------------------------
+    public function contentFormRelations()
+    {
+        return $this->hasMany(ContentFormRelation::class,
+            'vh_cms_content_form_field_id', 'id');
+    }
+
+}
