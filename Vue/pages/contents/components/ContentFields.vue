362a1e6528fa9b761afaa8032c9dfc1dd1bc1950
Kapil Singh <kapil-s001@webreinvent.com>
2023-11-09 12:48:48 +0530

    Update: font weight of labels

--- Vue/pages/contents/components/ContentFields.vue
+++ Vue/pages/contents/components/ContentFields.vue
@@ -63,7 +63,7 @@ const props = defineProps({
                      v-for="(field, f_index) in group.fields"
                      :key="f_index">
                     <div class="w-full flex flex-column gap-1">
-                        <label class="text-xs">{{ field.name }}</label>
+                        <label class="font-medium line-height-1 text-xs">{{ field.name }}</label>
                         <div v-if="!field.content || typeof field.content === 'string'
                         || typeof field.content === 'number'
                         || store.assets.non_repeatable_fields.includes(field.type.slug)"
